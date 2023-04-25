4484d7c25cf96f7890b6126ca25a6ed734e404a1
Hy-Quynh <hyquynh123@gmail.com>
2023-04-25 15:31:15 +0700

    fix background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function NavBar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL <i className="fab fa-typo3">
+            BOOTO <i className="fab fa-typo3">
             </i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
